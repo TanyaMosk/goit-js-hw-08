3cfe9c855ab18b7d881a75b87390f10ab3732f15
TanyaMosk <tanya856227@gmail.com>
2023-06-16 10:24:18 +0200

    Completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe, {
+  id: 'vimeo-player', 
+});
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// Отримуємо поточний час в секундах та зберігаємо в локальному сховищі
+function getCurrentTime(currentTime) {
+  const timeInSeconds = currentTime.seconds;
+  localStorage.setItem(STORAGE_TIME_KEY, JSON.stringify(timeInSeconds));
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_TIME_KEY)) ?? 0);
+
+// // Отримуємо поточний час відтворення та виводимо у консоль
+// player.on('pause', function () {
+//   player
+//     .getCurrentTime()
+//     .then(function (time) {
+//       console.log('Відео на паузі. Час відтворення:', time, 'сек');
+//     })
+//     .catch(function (error) {
+//       console.log('Помилка отримання часу відтворення:', error);
+//     });
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const { email, message } = refs;
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+controlData();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all fields');
+  }
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log('Відправлені дані:', formData);
+// console.log(`Відправлені дані:${JSON.stringify(formData)}`);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);  
+}
+
+function onFormInput() {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+function controlData() {
+  let savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    email.value = savedData.email;
+    message.value = savedData.message;
+    // console.log('Збережені дані:', savedData);
+  }  
+}
